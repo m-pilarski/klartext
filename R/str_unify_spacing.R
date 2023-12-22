7ebea31513f41fbe54adb9d879fe50f3437964c4
m-pilarski <pilarski@cryptolab.net>
2023-12-22 11:39:29 +0100

    update

--- R/str_unify_spacing.R
+++ R/str_unify_spacing.R
@@ -20,18 +20,18 @@
 #'   "www.example.com/test ..."
 #' ))
 str_unify_spacing <- function(.str){
-
+  
   .tok_lock_regex <- str_c(
     pattern_reg_url, pattern_reg_screen_name, pattern_reg_hashtag,
     pattern_reg_klartag, sep="|"
   )
 
-  .str %>%
-    stringi::stri_replace_all_regex(.tok_lock_regex, " $0 ") %>%
-    stringi::stri_split_charclass("\\p{WHITE_SPACE}", omit_empty=TRUE) %>%
-    tibble::as_tibble_col("tok") %>%
-    tibble::rowid_to_column("doc_id") %>%
-    # tidyr::unnest_longer(tok) %>%  this is extremely slow ... temp fix...
+  .str |>
+    stringi::stri_replace_all_regex(.tok_lock_regex, " $0 ") |>
+    stringi::stri_split_charclass("\\p{WHITE_SPACE}", omit_empty=TRUE) |>
+    tibble::as_tibble_col("tok") |>
+    tibble::rowid_to_column("doc_id") |>
+    # tidyr::unnest_longer(tok) |>  this is extremely slow ... temp fix...
     (\(.tbl){
       tibble::tibble(
         doc_id = rep(.tbl$doc_id, times=lengths(.tbl$tok)),
@@ -42,21 +42,21 @@ str_unify_spacing <- function(.str){
       condition=stringi::stri_detect_regex(tok, .tok_lock_regex),
       true=as.list(tok),
       false=stringi::stri_split_boundaries(tok, type="word")
-    )) %>%
-    # tidyr::unnest_longer(tok) %>%
+    )) |>
+    # tidyr::unnest_longer(tok) |>
     (\(.tbl){
       tibble::tibble(
         doc_id = rep(.tbl$doc_id, times=lengths(.tbl$tok)),
         tok = unlist(.tbl$tok)
       )
     })() |> 
-    dplyr::group_by(doc_id) %>%
+    dplyr::group_by(doc_id) |>
     dplyr::summarize(
       str = stringi::stri_c(tok, collapse=" "), .groups="drop"
-    ) %>%
-    tidyr::complete(doc_id = seq_along(.str), fill=list(str="")) %>%
-    dplyr::pull(str) %>%
-    rlang::set_names(names(.str))
+    ) |>
+    tidyr::complete(doc_id = seq_along(.str), fill=list(str="")) |>
+    dplyr::pull(str) |>
+    rlang::set_names(names(.str)) |> str()
 
 }
 
